5685f9c256bed8b4760132776100dae53e08f5af
Chris Dinn <chrisgdinn@gmail.com>
2012-05-31 19:03:23 -0400

    Clean up mongrel2 request example.

--- src/test/scala/smoke/mongrel2/Mongrel2RequestTest.scala
+++ src/test/scala/smoke/mongrel2/Mongrel2RequestTest.scala
@@ -6,9 +6,9 @@ import io.Source
 
 class Mongrel2RequestTest extends FunSpec {
   // Message Fixture (new lines added for readability):
-  // 54c6755b-9628-40a4-9a2d-cc82a816345e 57 /video_assets/A134/streams/B987 
-  // 713:{
-  //   "PATH":"/video_assets/A134/streams/B987",
+  // 54c6755b-9628-40a4-9a2d-cc82a816345e 57 /A134/B987 
+  // 751:{
+  //   "PATH":"/A134/B987",
   //   "accept-language":"en-US,en;q=0.8",
   //   "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.57 Safari/534.24",
   //   "host":"test.mdialog.com:6767",
@@ -20,8 +20,8 @@ class Mongrel2RequestTest extends FunSpec {
   //   "content-type":"application/x-www-form-urlencoded",
   //   "METHOD":"POST",
   //   "VERSION":"HTTP/1.1",
-  //   "URI":"/video_assets/A134/streams/B987?latitude=45.432&longitude=47.334",
-  //   "QUERY":"latitude=45.432&longitude=47.334",
+  //   "URI":"/A134/B987?someVal=45.432&other+val=47.334",
+  //   "QUERY":"someVal=45.432&other+val=47.334",
   //   "PATTERN":"/search/"
   // },22:greeting=hello+goodbye,
   val message = Source.fromURL(getClass.getResource("/mongrel2_request.txt"))
@@ -35,21 +35,21 @@ class Mongrel2RequestTest extends FunSpec {
     assert(request.connection === "57")
         
     assert(request.method === "POST")
-    assert(request.uri === new URI("/video_assets/A134/streams/B987?latitude=45.432&longitude=47.334"))
-    assert(request.path === "/video_assets/A134/streams/B987")
+    assert(request.uri === new URI("/A134/B987?someVal=45.432&other+val=47.334"))
+    assert(request.path === "/A134/B987")
     assert(request.hostWithPort === "test.mdialog.com:6767")
     assert(request.host === "test.mdialog.com")
     assert(request.port === 6767)
     assert(request.ip === "127.0.0.1")
-    assert(request.queryString === Some("latitude=45.432&longitude=47.334"))
+    assert(request.queryString === Some("someVal=45.432&other+val=47.334"))
     
     assert(request.contentType === Some("application/x-www-form-urlencoded"))
     assert(request.userAgent === Some("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.57 Safari/534.24"))
 
     assert(request.body === "greeting=hello+goodbye")
 
-    assert(request.params.get("latitude") === Some("45.432"))
-    assert(request.params.get("longitude") === Some("47.334"))
+    assert(request.params.get("someVal") === Some("45.432"))
+    assert(request.params.get("other val") === Some("47.334"))
     assert(request.params.get("greeting") === Some("hello goodbye"))
   }
   
