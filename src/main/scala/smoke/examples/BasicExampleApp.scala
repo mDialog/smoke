d340847e821066ae583fdadc29821d3689bc994c
Arron Norwell <anorwell@mdialog.com>
2012-07-24 12:42:17 -0400

    oops. fix basic example app.

--- src/main/scala/smoke/examples/BasicExampleApp.scala
+++ src/main/scala/smoke/examples/BasicExampleApp.scala
@@ -2,23 +2,7 @@ package smoke.examples
 
 import smoke._
 
-import akka.actor._
-import com.typesafe.config.ConfigFactory
-
-class TestActor extends Actor {
-  def receive = {
-    case _ ⇒
-  }
-}
-
 object BasicExampleApp extends Smoke {
-
-  override val config = ConfigFactory.load("configuration.properties")
-    .withFallback(ConfigFactory.load())
-
-  init()
-
-  val noop = system.actorOf(Props[TestActor], "test")
 
   onRequest {
     case GET(Path("/example")) ⇒ reply {
